79b0b6403c052577ac8b8bc154fed863b5e7fce9
Tim Egenton <tegenton@ucvts.tec.nj.us>
2018-11-20 13:55:44 -0500

    Add BankAccount class

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -8,6 +8,52 @@
  * for inspiration (https://github.com/rwilson-ucvts/java-sample-atm).
  */
 
+import java.security.InvalidParameterException;
+import java.util.Scanner;
+
 public class BankAccount {
-	
+    private double balance = 0;
+    private long accountNumber;
+    private User accountHolder;
+    private static long accountNum = 100000000;
+    
+    BankAccount(User accountHolder) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNum++;
+    }
+    public BankAccount(Scanner in) {
+    	accountHolder = new User(in);
+    }
+    
+    void deposit(double amount) {
+    	if (amount <= 0)
+    		throw new InvalidParameterException("Deposit amount is too low");
+        balance += amount;
+    }
+    void withdraw(double amount) {
+    	if (balance >= amount && amount > 0)
+    		balance -= amount;
+    	else if (0 > amount)
+    		throw new InvalidParameterException("Withdraw amount is too low");
+    	else
+    		throw new InvalidParameterException("Withdraw amount is too high");
+    }
+    void transfer(BankAccount reciever, double amount) {
+    	if (reciever == null)
+    		throw new InvalidParameterException("Recieving account does not exist");
+    	this.withdraw(amount);
+    	reciever.deposit(amount);
+    }
+    
+    double getBalance() {
+        return balance;
+    }
+    
+    long getAccountNumber() {
+        return accountNumber;
+    }
+    
+    User getUser() {
+        return accountHolder;
+    }    
 }
